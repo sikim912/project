b6423ba0ce471f220d487412c7ed46d0f4509a36
sikim912 <syouichi912@yahoo.co.jp>
2023-03-17 20:56:40 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,26 @@ const imgAttributes = ref({
   src: "/images/logo.svg",
   alt: "Vueのロゴ",
   width: 75,
-  height: 75,
+  height: 75
 });
+
 
 const url = ref("https://vuejs.org/");
 const isSendButtonDisabled = ref(true);
 
 const widthOrHeight = ref("height");
 const widthOrHeightValue = ref(50);
+
+
+
 </script>
 
 <template>
-  <p><img v-bind="imgAttributes" /></p>
+  <p><img v-bind="imgAttributes"></p>
 
   <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
   <p><a :href="url" target="_blank">Vue.jsのサイト(省略系)</a></p>
-  <p>
-    <a v-bind:href="url + 'guide/introduction.html'" target="_blank"
-      >Vue.jsガイドのページ</a
-    >
-  </p>
-  <p>
-    <button type="button" v-bind:disabled="isSendButtonDisabled">送信</button>
-  </p>
-  <p>
-    <img
-      alt="vueLogo"
-      src="./assets/logo.svg"
-      v-bind:[widthOrHeight]="widthOrHeightValue"
-    />
-  </p>
+  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
+  <p><button type="button" v-bind:disabled="isSendButtonDisabled">送信</button></p>
+  <p><img alt="vueLogo" src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue"></p>
 </template>
